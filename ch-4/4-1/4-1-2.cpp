52af99ea7b3bace784cbbb26d433064f6b6f7f33
krisha kakadiya 4563 <144050204+krishakakadiya@users.noreply.github.com>
2023-12-28 15:10:24 +0530

    Update 4-1-2.cpp

--- ch-4/4-1/4-1-2.cpp
+++ ch-4/4-1/4-1-2.cpp
@@ -1,8 +1,43 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
 using namespace std;
+class P
+{
+public:
+    double C;
+    double f;
+};
+class Q : public P
+{
+public:
+    double F;
+    void toFehrenheit()
+    {
+        cout << "enter any degree in Fehrenheit :";
+        cin >> f;
+        cout << "enter any degree in Celsius :";
+        cin >> C;
+
+        F = (C * 1.8) + 32;
+        cout << "Celsius to Fehrenheit :" << F << endl;
+    }
+};
+class R : public Q
+{
+public:
+    int kelvin;
+    void tokelvin()
+    {
+        kelvin = (F - 32) * 5/9  +  273;
+        cout << " Fehrenheit  to Kelvin :" << kelvin;
+    }
+};
 
 int main()
 {
-    
+    Q f1;
+    f1.toFehrenheit();
+
+    R k1;
+    k1.tokelvin();
 }
+    
